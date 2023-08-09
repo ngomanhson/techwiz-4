98a686cb507cb30b69129a52f0c8795fafdb914d
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2023-08-09 13:57:17 +0700

    database

---
+++ database/migrations/2023_08_09_050556_create_table_products.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('product_category_id')->unsigned();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('content')->nullable();
+            $table->decimal('price', 14,2);
+            $table->integer('qty');
+            $table->decimal('discount',14,2)->nullable();
+            $table->double('weight')->nullable();
+            $table->string('sku')->unique();
+            $table->boolean('featured');
+            $table->integer('tag_id')->unsigned();
+            $table->string("slug")->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2023_08_09_062427_create_table_product_review.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_review', function (Blueprint $table) {
+            $table->id();
+            $table->text("message");
+            $table->unsignedBigInteger("score");
+            $table->integer('user_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_review');
+    }
+};
