7aefbb53d103cee9265cc81f4670024a65fe713b
NgoManhson <ngomanhson2004txpt@gmail.com>
2023-08-09 08:51:04 +0700

    Test deploy

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::prefix('admin')->group(function (){
-    Route::get('dashboard',[\App\Http\Controllers\Admin\DashboardController::class,'index']);
+Route::get('/admin', function () {
+    return view('welcome');
 });
+// Route::prefix('admin')->group(function (){
+//     Route::get('dashboard',[\App\Http\Controllers\Admin\DashboardController::class,'index']);
+// });
