043dd5f8505ec529e05be9378c6695b06e2205bd
Hien Tran <109644638+hienttm@users.noreply.github.com>
2023-08-10 16:13:17 +0700

    AccountController

--- app/Http/Controllers/Front/AccountController.php
+++ app/Http/Controllers/Front/AccountController.php
@@ -3,86 +3,20 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use App\Repositories\User\UserRepositoryInterface;
-use App\Utilities\Constant;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 class AccountController extends Controller
 {
-    private $userService;
-
-
-    public function __construct(UserRepositoryInterface $userService)
-    {
-        $this->userService = $userService;
-    }
-    public function myAccount() {
-        return view('front.account.index');
-    }
-
-    public function checkLogin(Request $request) {
-        $request->validate([
-            "email" => "required|email",
-            "password" => "required",
-        ], [
-            "required" => "Please enter full information"
-        ]);
-        $credentials =[
-            'email'=> $request->email,
-            'password'=> $request->password,
-            'level'=> 2,
-        ];
-
-        if (Auth::attempt($credentials)) {
-            return redirect("/");
-        } else {
-            return back()->with('notification', 'ERROR: Email or password is wrong');
-        }
-    }
-
     public function login() {
-        return view('front.account.login');
+        return view("front.account.login");
     }
 
-    public function register() {
-        return view('front.account.register');
-    }
-    public function postRegister(Request $request) {
-
-        $request->validate([
-            "name"=>"required",
-            "email" => "required|email",
-            "password" => "required",
-        ], [
-            "required" => "Please enter full information"
-        ]);
-
-        $data=[
-            "name"=>$request->name,
-            "email"=>$request->email,
-            "password"=>bcrypt($request->password),
-            "level"=>Constant::user_level_client,
-        ];
-        $existingUser = User::where('email', $request->email)->first();
-
-        if ($existingUser) {
-            return redirect("/account/register")->with('notification', 'Email already exists.')->withInput();
-        }
-
-        try {
-            $this->userService->create($data);
-            return redirect("/account/login")->with('success', 'Register Success! Please login.');
-        } catch (\Exception $e) {
-            return redirect("/account/register")->with('notification', 'An error occurred. Please try again.')->withInput();
-        }
-    }
     public function logout() {
         Auth::logout();
 
         return back();
     }
-    public function orderDetail() {
-        return view('front.account.detail');
+
+    public function register() {
+        return view("front.account.register");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -83,11 +83,9 @@ Route::prefix('/checkout')->group(function (){
 });
 
 Route::prefix('account')->group(function () {
-    Route::get('my-account',[\App\Http\Controllers\Front\AccountController::class,'myAccount']);
+    Route::get('/',[\App\Http\Controllers\Front\AccountController::class,'myAccount']);
     Route::get('login',[\App\Http\Controllers\Front\AccountController::class,'login']);
-    Route::post('login',[\App\Http\Controllers\Front\AccountController::class,'checkLogin']);
     Route::get('register',[\App\Http\Controllers\Front\AccountController::class,'register']);
-    Route::post('register',[\App\Http\Controllers\Front\AccountController::class,'postRegister']);
     Route::get('logout',[\App\Http\Controllers\Front\AccountController::class,'logout']);
 });
 Route::prefix('/review')->group(function (){
