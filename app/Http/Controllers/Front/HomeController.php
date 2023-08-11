152154a397a63b1082c5d7b5146fba0907d9729a
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2023-08-11 10:48:49 +0700

    homecontroller

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     //
     public function index (){
-        return view("front.index");
+        $products_featured = Product::orderBy("id", "desc")->limit(10)->get();
+        return view("front.index", [
+            "products_featured" => $products_featured
+        ]);
     }
 }
