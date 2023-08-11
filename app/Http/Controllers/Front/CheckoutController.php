dfb5e177cd15d28200f6c100c71fa1916696308c
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2023-08-12 01:12:56 +0700

    vnpay controller

--- app/Http/Controllers/Front/CheckoutController.php
+++ app/Http/Controllers/Front/CheckoutController.php
@@ -195,10 +195,71 @@ class CheckoutController extends Controller
                     }
                 }
             }
+        } elseif ($order->payment_method == "VnPay") {
+            $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+            $vnp_Returnurl = "http://127.0.0.1:8000/checkout/nhan-ket-qua/".$order->id;
+            $vnp_TmnCode = "YNSXI5ZN";//Mã website tại VNPA
+            $vnp_HashSecret = "GKSNDWHVYQOWDPQQLHTQJCPQQVGUMIQL"; //Chuỗi bí mật
+
+            $vnp_TxnRef = $order->id; //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
+            $vnp_OrderInfo = 'Noi dung thanh toan';
+            $vnp_OrderType = 'billpayment';
+            $vnp_Amount = $total * 2300000;
+            $vnp_Locale = 'VN';
+            $vnp_BankCode = 'NCB';
+            $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
+            $inputData = array(
+                "vnp_Version" => "2.1.0",
+                "vnp_TmnCode" => $vnp_TmnCode,
+                "vnp_Amount" => $vnp_Amount,
+                "vnp_Command" => "pay",
+                "vnp_CreateDate" => date('YmdHis'),
+                "vnp_CurrCode" => "VND",
+                "vnp_IpAddr" => $vnp_IpAddr,
+                "vnp_Locale" => $vnp_Locale,
+                "vnp_OrderInfo" => $vnp_OrderInfo,
+                "vnp_OrderType" => $vnp_OrderType,
+                "vnp_ReturnUrl" => $vnp_Returnurl,
+                "vnp_TxnRef" => $vnp_TxnRef,
+            );
+
+            if (isset($vnp_BankCode) && $vnp_BankCode != "") {
+                $inputData['vnp_BankCode'] = $vnp_BankCode;
+            }
+            ksort($inputData);
+            $query = "";
+            $i = 0;
+            $hashdata = "";
+            foreach ($inputData as $key => $value) {
+                if ($i == 1) {
+                    $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
+                } else {
+                    $hashdata .= urlencode($key) . "=" . urlencode($value);
+                    $i = 1;
+                }
+                $query .= urlencode($key) . "=" . urlencode($value) . '&';
+            }
+
+            $vnp_Url = $vnp_Url . "?" . $query; // gheps
+            if (isset($vnp_HashSecret)) {
+                $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret);//
+                $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash; // ghép tghanhf 1 url để đi sang vnpay
+            }
+            header("Location: ".$vnp_Url);// đi sang bên vnpay
+            die();// xong việc của việc gửi thông tin thanh toán
         }
 
         // Send Email
         $this->sendEmail($request ,$order);
+        return redirect("/checkout/thank-you/")->with("notification","Success! You have successfully paid for your order. Please check your email.");
+    }
+
+    public function nhanKetQua(Order $order,Request $request)
+    {
+        if ($request->get("vnp_ResponseCode") == "00") {
+            $order->update(["is_paid" => true, "status" => 1]);// đã thanh toán, trạng thái về xác nhận
+            return redirect("/checkout/thank-you/")->with("notification","Success! You have successfully paid for your order. Please check your email.");
+        }
         return redirect("/checkout/thank-you/")->with("notification","Success! You have successfully paid for your order. Please check your email.");
     }
 
