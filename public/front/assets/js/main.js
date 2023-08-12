e29b42f807e74ffa655e7a859c17c5357e0e400e
trung-2004 <110414167+trung-2004@users.noreply.github.com>
2023-08-12 19:14:44 +0700

    js

--- public/front/assets/js/main.js
+++ public/front/assets/js/main.js
@@ -817,3 +817,20 @@ function updateCart(rowId , qty){
     });
 
 }
+function confirmation(ev) {
+    ev.preventDefault();
+    var urlToRedirect = ev.currentTarget.getAttribute('href');
+    console.log(urlToRedirect);
+    swal({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willCancel) => {
+            if (willCancel) {
+                window.location.href = urlToRedirect;
+            }
+        });
+}
